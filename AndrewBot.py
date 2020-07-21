65dac5724bc5dae6e53555e8fa0425309943afe2
AndrewTheDrummer <68514813+AndrewTheDrummer@users.noreply.github.com>
2020-07-21 21:53:12 +0300

    Add files via upload

---
+++ AndrewBot.py
@@ -0,0 +1,33 @@
+
+import discord 
+from discord.ext import commands 
+import os
+
+client = commands.Bot(command_prefix = '.')
+
+@client.event 
+async def on_ready():
+    print('Bot is ready')
+    await client.change_presence(activity=discord.Activity(name="Breaking Bad", type=discord.ActivityType.watching))
+
+@client.command()
+@commands.is_owner()
+async def load(ctx, extenstion):
+    client.load_extension(f'commands.{extenstion}')
+
+@client.command()
+@commands.is_owner()
+async def unload(ctx, extenstion):
+    client.unload_extension(f'commands.{extenstion}')
+
+@client.command()
+@commands.is_owner()
+async def reload(ctx, extenstion):
+    client.unload_extension(f'commands.{extenstion}')
+    client.load_extension(f'commands.{extenstion}')
+
+for filename in os.listdir('./commands'):
+    if filename.endswith('.py'):
+        client.load_extension(f'commands.{filename[:-3]}')
+
+client.run('NzM0NDcxMDYxODQ0MDAwODE4.XxSLiw.NNutn5_4IPLoVkmXBzUk1I1lLgM')
